01c078bf6915ce64ebfafc7132b909a26223a61b
torte <torte478@gmail.com>
2016-12-14 23:12:48 +0300

    feature (Qps) add status code check

--- src/UrlShort/qps.hs
+++ src/UrlShort/qps.hs
@@ -5,11 +5,15 @@ import Network.HTTP.Client.TLS   (tlsManagerSettings)
 import Network.HTTP.Types.Status (statusCode)
 import Data.ByteString.Lazy.Internal
 
-getShortUrl :: [Char] -> IO ByteString
+getShortUrl :: [Char] -> IO (Maybe ByteString)
 getShortUrl url = do
     manager <- newManager tlsManagerSettings
 
     request <- parseRequest $ "http://qps.ru/api?url=" ++ url
     response <- httpLbs request manager
 
-    return $ responseBody response
+    let status = statusCode $ responseStatus response
+    print status
+    if status == 200
+        then return $ Just $ responseBody response
+        else return Nothing
